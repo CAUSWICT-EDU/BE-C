99931c58587ee93149f9f48b05c58b8d55b71c3e
jihuuuu <jihuu2806@gmail.com>
2024-03-27 18:25:12 +0900

    rretry

--- jihu/week_1/src/week_1.java
+++ jihu/week_1/src/week_1.java
@@ -1,24 +1,28 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class week_1 {
     public static void main(String[] args) {
         int[] input = new int[3];
-        int num = 1, strike = 0, ball = 0;
+        int num = 1;
         Scanner sc = new Scanner(System.in);
         int[] computer = {1, 2, 3};
 
-        while (strike != 3) { // 숫자 야구 게임 시작
-            strike = 0;
-            ball = 0;
+        do { // 숫자 야구 게임 시작
+            int strike = 0, ball = 0;
 
             System.out.println("["+ num + "회차 숫자야구]\n");
             for (int i=0;i<3;i++) { // 사용자에게 숫자를 3번 입력 받는다
-                System.out.println("1에서 9까지의 숫자를 입력하세요 : ");
-                input[i] = sc.nextInt();
+                boolean checkinput = false;
 
-                if (input[i] < 1 || input[i] > 9) { // 1~9까지의 숫자가 아닌 경우 예외 처리
-                    System.out.println("숫자의 범위를 벗어났습니다! 1에서 9까지의 숫자를 다시 입력하세요");
-                    break;
+                while (!checkinput) {
+                    System.out.println("1에서 9까지의 숫자를 입력하세요 : ");
+                    input[i] = sc.nextInt();
+
+                    if (input[i] < 1 || input[i] > 9) { // 1~9까지의 숫자가 아닌 경우 예외 처리
+                        System.out.println("숫자의 범위를 벗어났습니다! 1에서 9까지의 숫자를 다시 입력하세요");
+                    } else {
+                        checkinput = true; // 숫자의 범위가 맞으면 예외 확인을 끝낸다
+                    }
                 }
             }
 
@@ -41,13 +45,20 @@ public class week_1 {
                 }
 
                 System.out.println("[결과]스트라이크 : "+ strike + ", 볼 : " + ball + "\n"); // 결과 출력
-
+                if (strike == 3) {
+                    break;
+                }
             }
 
-            num++; //
+            num++;
 
+        } while (num < 10);
+
+        if (num > 9) { // 결과 출력
+            System.out.println("9회차 게임이 모두 끝났습니다. 컴퓨터의 승리입니다.");
         }
-
-        System.out.println("정답입니다! 축하합니다!"); // 숫자 야구 끝
+        else {
+            System.out.println("정답입니다! 축하합니다!");
+        }
     }
 }
