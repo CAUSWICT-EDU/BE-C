b1e5e17ca2ff457f6fbd819997bccc0bf7fd12e1
dlwlsrud815 <dlwlsrud815@gmail.com>
2024-03-28 15:29:55 +0900

    FEAT: 사용자 입력 함수 구현

--- jingeong/week_2/java-baseball-precourse/src/main/java/baseball/User.java
+++ jingeong/week_2/java-baseball-precourse/src/main/java/baseball/User.java
@@ -4,12 +4,11 @@ import camp.nextstep.edu.missionutils.Console;
 public class User {
 
     public User() {
-        enterUserInput();
-
+        enterUserInput(); // User instance가 생성되는 동시에 Input 함수 호출
     }
 
     public String[] enterUserInput() {
-
+        return Console.readLine().split(""); // 주어진 라이브러리 임포트 후 사용
     }
 
 
