b31727c714958b620061b191433f26c06c51a02e
dlwlsrud815 <dlwlsrud815@gmail.com>
2024-03-29 13:07:50 +0900

    TEST: random test 수정

--- jingeong/week_2/java-baseball-precourse/src/main/java/baseball/Application.java
+++ jingeong/week_2/java-baseball-precourse/src/main/java/baseball/Application.java
@@ -7,10 +7,3 @@ public class Application {
     }
 }
 
-/*
-내일 고쳐볼거
-1. pickNumberInRange()로 랜덤 숫자 뽑기 다시 구현
-2. ExceptionInInitializerError 이 에러가 버전 문제인지, 아니면 코드를 잘못짠건지 알기
-3. 예외처리 방식 다시 생각 해보기
-
- */
--- jingeong/week_2/java-baseball-precourse/src/test/java/baseball/RandomTest.java
+++ jingeong/week_2/java-baseball-precourse/src/test/java/baseball/RandomTest.java
@@ -1,19 +1,29 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import javafx.scene.effect.SepiaTone;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class RandomTest {
-    Computer computer = new Computer();
 
     @Test
-    void pickRandomNuber() {
-        assertThat(computer.makeRandomNumber()).containsAnyElementsOf(Arrays.asList(1,2,3,4,5,6,7,8,9));
-        System.out.println(computer.makeRandomNumber());
+    void bound() {
+        assertThat(Computer.getInstance().makeRandomNumber()).containsAnyElementsOf(Arrays.asList(1,2,3,4,5,6,7,8,9));
+        System.out.println(Computer.getInstance().makeRandomNumber());
+    }
+
+    @Test
+    void duplicated() {
+        List<Integer> random = new ArrayList<>(Computer.getInstance().makeRandomNumber());
+        System.out.println(random);
+        Set<Integer> duple = new LinkedHashSet<>(random);
+        assertThat(duple.size()).isEqualTo(random.size());
+
+
     }
 }
 
