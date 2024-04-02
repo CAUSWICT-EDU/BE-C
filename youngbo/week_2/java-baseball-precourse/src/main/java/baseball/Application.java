d8e01725811d45a9b768fcf45d089dc5c30e2981
Hossmot <83452955+Hossmot@users.noreply.github.com>
2024-04-03 05:43:19 +0900

    FEAT: Application.java 구현

--- youngbo/week_2/java-baseball-precourse/src/main/java/baseball/Application.java
+++ youngbo/week_2/java-baseball-precourse/src/main/java/baseball/Application.java
@@ -1,7 +1,12 @@
 package baseball;
 
 public class Application {
-    public static void main(String[] args) {
-        //TODO: 숫자 야구 게임 구현
+
+  public static void main(String[] args) {
+    GameMaster GM = GameMaster.getGM(); //게임의 작동을 총괄하는 GM
+    NumBaseBall game = new NumBaseBall(); //새 야구게임 생성
+    while (true) {
+      game.newGame(GM, game); //게임 시작
     }
+  }
 }
