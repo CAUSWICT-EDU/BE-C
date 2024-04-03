ded164bf56854af314e641baa7788d4470bc20b8
Hossmot <83452955+Hossmot@users.noreply.github.com>
2024-04-03 16:51:31 +0900

    FEAT: Refree 인스턴스를 Application에서 생성하도록 변경

--- youngbo/week_2/java-baseball-precourse/src/main/java/baseball/Application.java
+++ youngbo/week_2/java-baseball-precourse/src/main/java/baseball/Application.java
@@ -5,8 +5,9 @@ public class Application {
   public static void main(String[] args) {
     GameMaster GM = GameMaster.getGM(); //게임의 작동을 총괄하는 GM
     NumBaseBall game = new NumBaseBall(); //새 야구게임 생성
+    Referee referee = new Referee();  //심판 생성
     while (true) {
-      game.newGame(GM, game); //게임 시작
+      game.newGame(GM, referee); //게임 시작
     }
   }
 }
