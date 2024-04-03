099549872cf6f0ecf1de3390991a2258f99050e7
Hossmot <83452955+Hossmot@users.noreply.github.com>
2024-04-03 17:00:59 +0900

    FEAT: NumBaseBall 클래스에서 Referee 객체를 생성하도록 변경하고, printResult에 답을넘겨줄 수 있도록 본 클래스를 인수로 추가했습니다.

--- youngbo/week_2/java-baseball-precourse/src/main/java/baseball/NumBaseBall.java
+++ youngbo/week_2/java-baseball-precourse/src/main/java/baseball/NumBaseBall.java
@@ -10,7 +10,7 @@ public class NumBaseBall {
     this.setAnswer();
   }
 
-  private void setAnswer() {  //답 생성
+  public void setAnswer() {  //답 생성
     compAnswer[0] = Randoms.pickNumberInRange(1, 9);
     while (compAnswer[0] == compAnswer[1] || compAnswer[1] == compAnswer[2]
         || compAnswer[2] == compAnswer[0]) {
@@ -23,12 +23,11 @@ public class NumBaseBall {
     return this.compAnswer; //컴퓨터가 정한 답을 반환한다.
   }
 
-  public void newGame(GameMaster GM, NumBaseBall NB) {  //새 게임 생성
-    Referee rf = new Referee();
-    rf.receiveAnswer(); //입력 받기
-    rf.refineAnswer();  //정제하기
-    rf.countResult(getAnswer());  //스트라이크, 볼 세기
-    rf.printResult(GM, NB); //결과 출력
+  public void newGame(GameMaster GM, Referee RF) {  //새 게임 생성
+    RF.receiveAnswer(); //입력 받기
+    RF.refineAnswer();  //정제하기
+    RF.countResult(getAnswer());  //스트라이크, 볼 세기
+    RF.printResult(GM, this); //결과 출력
   }
 
 }
