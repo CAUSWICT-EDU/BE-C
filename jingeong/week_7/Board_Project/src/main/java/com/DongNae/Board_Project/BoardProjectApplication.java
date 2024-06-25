6ea6a8ce76ff4cdead39ec2f0af0ea1d7e9432d3
dlwlsrud815 <dlwlsrud815@gmail.com>
2024-06-26 00:53:37 +0900

    fix conflict

---
+++ jingeong/week_7/Board_Project/src/main/java/com/DongNae/Board_Project/BoardProjectApplication.java
@@ -0,0 +1,15 @@
+package com.DongNae.Board_Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+// 시큐리티 일단 꺼놓기
+public class BoardProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoardProjectApplication.class, args);
+    }
+
+}
